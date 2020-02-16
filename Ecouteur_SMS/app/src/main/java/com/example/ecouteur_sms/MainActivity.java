6dc025d2746a9a845e5f6d4ced65932151104b32
Elmokhtar BENFRAJ <benfraj.elmokhtar@gmail.com>
2020-02-16 13:17:07 +0100

    + SMS Receiver update 16022020 (with customised permission)

--- Ecouteur_SMS/app/src/main/java/com/example/ecouteur_sms/MainActivity.java
+++ Ecouteur_SMS/app/src/main/java/com/example/ecouteur_sms/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.ecouteur_sms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    Switch sw_receive;
+    Button btn_ath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sw_receive = (Switch) findViewById(R.id.sw_receive);
+        btn_ath = (Button) findViewById(R.id.btn_ath);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)== PackageManager.PERMISSION_DENIED){
+
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECEIVE_SMS},0);
+               sw_receive.setChecked(false);
+        }   else {
+            sw_receive.setChecked(true);
+        }
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)== PackageManager.PERMISSION_DENIED){
+            sw_receive.setChecked(false);
+        }   else {
+            sw_receive.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 }
